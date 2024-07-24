93edb200f78235c339fa52677f81833927475235
Victor Raony <victor_raony12@hotmail.com>
2024-07-24 01:17:43 -0300

    Config: Implementing components Loading and Input

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -9,8 +9,19 @@ import {
         Inter_500Medium, 
         Inter_600SemiBold } from "@expo-google-fonts/inter"
 
+import { Loading } from "../components/loading"
 
 export default function Layout() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_600SemiBold
+  })
+
+  if(!fontsLoaded){
+    return <Loading />
+  }
+
   return (
   <View className="bg-zinc-900 flex-1">
     <StatusBar 
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,20 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
+
+import { Input } from '@/src/components/input'
 
 export default function Index(){
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className='text-zinc-400'>
-        Convide seus amigos e planeje suas viagens
+    <View className="flex-1 items-center justify-center px-5">
+      <Image source={require('@/src/assets/logo.png')} className='h-8' resizeMode='contain'/>
+      <Text className='text-zinc-400 font-regular text-center text-lg mt-3'>
+          Convide seus amigos e planeje suas{"\n"}proxima viagem
       </Text>
+
+      <View className='w-full bg-zinc-900 p-4 rounded-xl my-8 border border-zinc-800'>
+        <Input >
+          <Input.Field placeholder='Para onde?'/>
+        </Input>
+      </View>
     </View>
   )
 }
